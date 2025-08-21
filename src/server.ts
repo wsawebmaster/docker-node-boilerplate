4c3aea374c5b64d57201ce72cd87eb19b7e17ff9
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-08-21 17:13:28 +0000

    feat: instalado dependências, importado configurações dev container e primeiro endpoint fastAPI

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import fastify from "fastify";
+import { request } from "http";
+
+const server = fastify({ logger: true });
+
+server.get("/teams", async(request, response)=>{
+    response.type("application/json").code(200)
+
+    return [{ id: 1, name: "ferrari" }];
+});
+
+server.listen({port: 3333}, ()=>{
+    console.log("Server init");
+})
