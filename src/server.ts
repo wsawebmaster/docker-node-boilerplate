fa6b0415ac5326c4fbd7cc4cfd6e69396dbc3afb
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-08-21 17:30:14 +0000

    feat: Adicionado mais enpoints

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,29 @@ import { request } from "http";
 
 const server = fastify({ logger: true });
 
-server.get("/teams", async(request, response)=>{
-    response.type("application/json").code(200)
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+]
 
-    return [{ id: 1, name: "ferrari" }];
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 3, name: "Lando Norris", team: "McLaren" },
+];
+
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+
+  return [{ teams }];
 });
 
-server.listen({port: 3333}, ()=>{
-    console.log("Server init");
-})
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200);
+  return [{ drivers }];
+});
+
+server.listen({ port: 3333 }, () => {
+  console.log("Server init");
+});
