b104849cdae59c4f3b5bcb4df58db515e55dec6a
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-08-21 18:06:37 +0000

    feat: adicionado filtros por id

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,15 @@ const teams = [
   { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
   { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
   { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 7, name: "Alfa Romeo Racing", base: "Hinwil, Switzerland" },
+  { id: 8, name: "AlphaTauri", base: "Faenza, Italy" },
+  { id: 9, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 10, name: "Haas", base: "Kannapolis, United States" },
+  { id: 11, name: "Uralkali Haas F1 Team", base: "Banbury, United Kingdom" },
+  { id: 12, name: "Scuderia Toro Rosso", base: "Faenza, Italy" },
 ]
 
 const drivers = [
@@ -25,6 +34,26 @@ server.get("/drivers", async (request, response) => {
   response.type("application/json").code(200);
   return [{ drivers }];
 });
+
+interface DriverParams {
+  id: string;
+}
+
+server.get<{ Params: DriverParams }>(
+  "/drivers/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find((d) => d.id === id);
+
+    if (!driver) {
+      response.type("application/json").code(404);
+      return { message: "Driver Not Found" };
+    } else {
+      response.type("application/json").code(200);
+      return { driver };
+    }
+  }
+);
 
 server.listen({ port: 3333 }, () => {
   console.log("Server init");
